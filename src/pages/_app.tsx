f7df77c304f539007b7988757e88c882f4d1b1b2
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-03-16 23:59:17 -0300

    refactor: configuração inicial do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,26 @@
-import { AppProps } from 'next/app'
-import Head from 'next/head'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-import GlobalStyles from 'styles/global'
+import GlobalStyles from 'styles/global';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>My Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simples project to show my favorite spot in the world."
         />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

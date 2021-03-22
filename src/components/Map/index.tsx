92e93bebc59da209b6512d4845960625f7195c3a
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-03-22 11:39:07 -0300

    fix: definindo zoom minimo e limites do mapa

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -46,6 +46,11 @@ const Map = ({ places }: MapProps) => {
         center={[51.505, -0.09]}
         zoom={3}
         style={{ height: '100%', width: '100%' }}
+        minZoom={3}
+        maxBounds={[
+          [-180, 180],
+          [180, -180],
+        ]}
       >
         <CustomTileLayer />
 

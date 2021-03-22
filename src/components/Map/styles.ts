ea8a60ad5f24268f0147dace16b5d7e095bf44fd
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-03-22 11:15:02 -0300

    fix: removendo espaços vazios em cincza

--- src/components/Map/styles.ts
+++ src/components/Map/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.main``;
+export const MapWrapper = styled.main`
+  width: 100%;
+  height: 100%;
+  .leaflet-container {
+    background-color: var(--background);
+  }
+`;
